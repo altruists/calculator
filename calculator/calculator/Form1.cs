2e5d68502a29c081e7fe4e63dee9e441492c2962
altruists <ann_nau@mail.ru>
2016-02-25 14:54:13 +0600

    add calc with +

---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstArg = Convert.ToDouble(textBox1.Text);
+            double secArg = Convert.ToDouble(textBox2.Text);
+            double result = firstArg + secArg;
+            label1.Text = result.ToString();
+
+        }
+
+    }
+}
