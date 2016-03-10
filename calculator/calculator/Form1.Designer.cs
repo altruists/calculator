90b15d589d414e10c2fc68ffd24039bcaff050cb
altruists <ann_nau@mail.ru>
2016-03-10 14:20:38 +0600

    Calc REFACTORING

--- calculator/calculator/Form1.Designer.cs
+++ calculator/calculator/Form1.Designer.cs
@@ -70,7 +70,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Calculate);
             // 
             // button2
             // 
@@ -81,7 +81,7 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "*";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Calculate);
             // 
             // button3
             // 
@@ -92,7 +92,7 @@
             this.button3.TabIndex = 5;
             this.button3.Text = "/";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.Calculate);
             // 
             // button4
             // 
@@ -103,7 +103,7 @@
             this.button4.TabIndex = 6;
             this.button4.Text = "-";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Click += new System.EventHandler(this.Calculate);
             // 
             // Form1
             // 
--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -49,6 +49,40 @@ namespace calculator
             double result = firstArg / secArg;
             label1.Text = result.ToString();
         }
+        private void Calculate(object sender, EventArgs e)
+        {
+            double firstArg = Convert.ToDouble(textBox1.Text);
+            double secArg = Convert.ToDouble(textBox2.Text);
+            double result;
+            switch (((Button) sender).Name)
+            {
+                case "button1":
+                {
+                    result = firstArg + secArg;
+                    break;
+                }
+                case "button2":
+                {
+                    result = firstArg * secArg;
+                    break;
+                }
+                case "button3":
+                {
+                    result = firstArg / secArg;
+                    break;
+                }
+                case "button4":
+                {
+                    result = firstArg - secArg;
+                    break;
+                }
+                default : throw new Exception("Sorry you are not winner");
+
+            }
+
+            label1.Text = result.ToString();
+
+        }
 
     }
 }
